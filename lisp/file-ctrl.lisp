de6dac4378097fa1e18ba16d04add0e393a74caa
Seymour Makarov <seymour.makarov@gmail.com>
2016-02-24 01:12:46 +0300

    Orienting on YAML format. Programmed the first macro for "DEFINE-MACRO" definition

--- lisp/file-ctrl.lisp
+++ lisp/file-ctrl.lisp
@@ -16,11 +16,48 @@
            (t
             (return path))))))
 
+(defun walk-through-hash (hash)
+;;  (format t "Type: ~a;" (type-of hash))
+  (let ((result '()))
+    ;;(dolist (hash list-of-hashes)
+      (cond
+        ((eql (type-of hash) 'HASH-TABLE)
+         (loop for key being the hash-keys of hash
+                 using (hash-value value)
+               do
+                  (cond
+                    ((eql (type-of value) 'CONS)
+                     (format t "Type: ~a~%" (type-of value))
+                     (return-from walk-through-hash (append result (cons key (walk-through-hash value))))
+                     )
+                    (t
+                     (format t "Another-2: ~a~%" (type-of value))))))
+        (t
+         (format t "Another-1: ~a~%" (type-of hash))
+         (return-from walk-through-hash (list hash))
+         ))))
+
+
 (defun read-config (filename)
   (let ((doc (cl-yy::yaml-load-file filename)))
     (dolist (hash doc)
       (loop for key being the hash-keys of hash
-            do (print (gethash key hash))))))
+              using (hash-value value)
+            do
+               (cond
+                 ((string= key "productions")
+                  (walk-through-hash (gethash "productions" hash)))
+                 (t
+                  (format t "Key: ~a~%" key)))))))
+;;
+;;       (loop for key being the hash-keys of hash
+;;             do
+;; ;               (format t "Key: ~a. Value: ~a~%" key (gethash key hash))))))
+;;                (dolist (h (gethash key hash))
+;;                  (loop for k being the hash-keys of h
+;;                        do
+;;                           (format t "Key: ~a. Value: ~a~%" k (gethash k h)))))))
+  )
 
   ;; (with-open-file (stream filename)
 
--- lisp/parser-def.lisp
+++ lisp/parser-def.lisp
@@ -5,22 +5,10 @@
 
 ;;(defvar *term-list* '(terminals productions precedence start-symbol))
 
-(defmacro make-parser-macro (name terminals start-symbol productions &optional precedence)
-;  (declare (symbol start-symbol))
-  `(make-gramma ,name
-     (:start-symbol ,start-symbol)
-;     (:terminals terminals)
-;     ,(if precedence
-;          '(:precedence precedence))
-     (productions)
-  ))
-
-
-(defmacro define-parse (name &body body)
-  `(defparameter ,name
-     (def-parser ,@body)))
-
-(defmacro do-primes ((var start end) &body body)
-  `(do ((,var (next-prime ,start) (next-prime (1+ ,var))))
-       ((> ,var ,end))
-     ,@body))
+(defmacro make-parser-macro (&key name terminals start-symbol productions precedence)
+  `(define-parser ,name
+      (:start-symbol ,start-symbol)
+    (:terminals ,terminals)
+    ,productions
+    )
+  )
